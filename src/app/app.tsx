975523c516989b4a3434e322a761b045aa7ee663
Alexander Mandryk <amandryk@Alexanders-MacBook-Pro.local>
2017-05-18 00:09:03 +0300

    Event

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,7 +5,7 @@ export interface IAppProps {
     game: string;
 }
 
-export interface IAppState{
+export interface IAppState {
     result1: number,
     result2: number
 }
@@ -17,15 +17,47 @@ export default class App extends React.Component<IAppProps, IAppState> {
         this.state = {
             result1: 0,
             result2: 0
-        }
+        };
+
+        this.onPlayer2Win = this.onPlayer2Win.bind(this);
+        this.onPlayer2Loose = this.onPlayer2Loose.bind(this);
+    }
+
+    onPlayer2Win(){
+        this.setState({ result2: this.state.result2 + 1 });
+    }
+
+    onPlayer2Loose(){
+        this.setState({ result2: this.state.result2 - 1 });
     }
 
     render() {
+        const winner = this.state.result1 == this.state.result2
+            ? null
+            : this.state.result1 > this.state.result2 ? "Player 1" : "Player 2";
+
         return (
             <div>
-                <h1 onClick={() => this.setState({result1: this.state.result1 + 2 , result2: this.state.result2 +1})}>{this.props.game} result:</h1>
-                <Player name="Player 1" result={this.state.result1}/>
-                <Player name="Player 2" result={this.state.result2}/>
+                <h1>{this.props.game} result:</h1>
+                {winner && <section>
+                    <h2>Winner {winner}</h2>
+                </section>}
+                <section>
+                    <Player
+                        name="Player 1"
+                        result={this.state.result1}
+                        onWin={() => this.setState({ result1: this.state.result1 + 1 })}
+                        onLoose={() => this.setState({ result1: this.state.result1 - 1 })}
+                    />
+                </section>
+                <section>
+                    <Player
+                        name="Player 2"
+                        result={this.state.result2}
+                        onWin={this.onPlayer2Win}
+                        onLoose={this.onPlayer2Loose}
+                    />
+                </section>
                 <section>
                     {this.props.children}
                 </section>
