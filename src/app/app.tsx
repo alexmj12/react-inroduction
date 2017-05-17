13e206da29c8fbdea9cdf2b6dc9b168fe1380072
Alexander Mandryk <amandryk@Alexanders-MacBook-Pro.local>
2017-05-17 23:47:23 +0300

    State

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,17 +5,27 @@ export interface IAppProps {
     game: string;
 }
 
-export default class App extends React.Component<IAppProps, {}> {
+export interface IAppState{
+    result1: number,
+    result2: number
+}
+
+export default class App extends React.Component<IAppProps, IAppState> {
     constructor(props: IAppProps, context: any) {
         super(props, context);
+
+        this.state = {
+            result1: 0,
+            result2: 0
+        }
     }
 
     render() {
         return (
             <div>
-                <h1>{this.props.game} result:</h1>
-                <Player name="Player 1" result={0}/>
-                <Player name="Player 2" result={0}/>
+                <h1 onClick={() => this.setState({result1: this.state.result1 + 2 , result2: this.state.result2 +1})}>{this.props.game} result:</h1>
+                <Player name="Player 1" result={this.state.result1}/>
+                <Player name="Player 2" result={this.state.result2}/>
                 <section>
                     {this.props.children}
                 </section>
