cafe0ef7c259eef585c08820d3cac8b19195d792
alexmj12 <alexmj12.1981@gmail.com>
2017-06-08 17:46:09 +0300

    Add action values and devtools

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,28 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 
 import * as Actions from "./actions";
 import counter from "./reducers";
 import App from "./app";
 
-const store = createStore(counter);
+const store = createStore(counter, /* preloadedState, */
+   window["__REDUX_DEVTOOLS_EXTENSION__"] && window["__REDUX_DEVTOOLS_EXTENSION__"]()
+   );
+
 
 const renderApp = () => render(
-  <App 
-    game="Game 1"
-    result={store.getState()}
-    onPlayer1Won={() => store.dispatch({ type: Actions.PLAYER1_WON})}
-    onPlayer1Loose={() => store.dispatch({ type: Actions.PLAYER1_LOOSE})}
-    onPlayer2Won={() => store.dispatch({ type: Actions.PLAYER2_WON})}
-    onPlayer2Loose={() => store.dispatch({ type: Actions.PLAYER2_LOOSE})}
+    <App
+        game="Game 1"
+        result={store.getState()}
+        onPlayer1Won={() => store.dispatch({ type: Actions.PLAYER1_WON, value: 1 })}
+        onPlayer1Loose={() => store.dispatch({ type: Actions.PLAYER1_LOOSE, value: 1 })}
+        onPlayer2Won={() => store.dispatch({ type: Actions.PLAYER2_WON, value: 1 })}
+        onPlayer2Loose={() => store.dispatch({ type: Actions.PLAYER2_LOOSE, value: 1 })}
     >
         <h5>Copiright (c) 2017</h5>
     </App>,
-  document.getElementById("root")
+    document.getElementById("root")
 );
 
 renderApp();
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -9,26 +9,26 @@ export default (state = { player1: 0, player2: 0 }, action) => {
     switch (action.type) {
         case Actions.PLAYER1_WON:
             return {
-                player1: state.player1 + 1,
+                player1: state.player1 + action.value,
                 player2: state.player2
             }
         
         case Actions.PLAYER1_LOOSE:
             return {
-                player1: state.player1 - 1,
+                player1: state.player1 - action.value,
                 player2: state.player2
             }
             
         case Actions.PLAYER2_WON:
             return {
                 player1: state.player1,
-                player2: state.player2 + 1
+                player2: state.player2 + action.value
             }
         
         case Actions.PLAYER2_LOOSE:
             return {
                 player1: state.player1,
-                player2: state.player2 - 1
+                player2: state.player2 - action.value
             }
 
         default:
