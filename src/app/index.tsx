239946ab0d8e1c7ce3821ac57e71d91e73ae8c0b
Alexander Mandryk <amandryk@Alexanders-MacBook-Pro.local>
2017-06-09 01:25:32 +0300

    Add tools

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,15 +2,20 @@ import * as React from "react";
 import { render } from "react-dom";
 import { useStrict } from 'mobx';
 import { Provider } from 'mobx-react';
+import { connectReduxDevtools } from "mobx-state-tree";
+import * as remotedev from "remotedev";
 
 import AppStore, { APP_STORE } from "./stores/appStore";
 import App from "./app";
 
 useStrict(true);
 
+const appStore = AppStore.create({});
 const stores = {
-    [APP_STORE]: AppStore.create({})
+    [APP_STORE]: appStore
 };
+
+connectReduxDevtools(remotedev, appStore);
 
 render(
     <Provider {...stores}>
